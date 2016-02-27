c45be30a9fa465805e28392b416095e86df3b8af
dathi <dathi@tma.com.vn>
2016-02-27 13:57:10 +0700

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107094438) do
+ActiveRecord::Schema.define(version: 20160227065640) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20160107094438) do
     t.string   "poster_url"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
